7bebe93826f33a23fb015ee628b9fe76a9dd4e63
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2020-01-07 14:23:19 +0700

    Fix getting version

--- thunagen/__init__.py
+++ thunagen/__init__.py
@@ -1,3 +1,24 @@
-from importlib_metadata import version
+import re
+import sys
+from pathlib import Path
 
-__version__ = version('thunagen')
+
+pyver = (sys.version_info.major, sys.version_info.minor)
+if pyver <= (3, 7):
+    import importlib_metadata
+else:
+    import importlib.metadata as importlib_metadata
+
+
+def get_version():
+    try:
+        return importlib_metadata.version('thunagen')
+    except importlib_metadata.PackageNotFoundError:
+        pass
+    # Run from development working folder
+    filepath = Path(__file__).parent.parent / 'pyproject.toml'   # type: Path
+    match = re.search('version = "([.0-9]+)"', filepath.read_text())
+    return match.group(1)
+
+
+__version__ = get_version()
