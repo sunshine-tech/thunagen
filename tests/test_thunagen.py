1e07825c5c9f9bae9347b8bc52c13be9b3b6f387
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2020-02-04 08:52:23 +0700

    Add test case with file missing extension

--- tests/test_thunagen.py
+++ tests/test_thunagen.py
@@ -1,13 +1,22 @@
 import shutil
 from pathlib import Path
 
+import pytest
 from PIL import Image
 
 from thunagen.common import ImgSize
 from thunagen.functions import create_thumbnail
 
 
-def test_create_thumbnail(tmp_path):
+testdata = (
+    ('annie-spratt-NlcSjubZ9tM-unsplash.jpg', 'thumbnails/annie-spratt-NlcSjubZ9tM-unsplash_512x512.jpg'),
+    # This file is missing extension
+    ('patrick-hendry-CKFgnLGpWqc-unsplash', 'thumbnails/patrick-hendry-CKFgnLGpWqc-unsplash_512x512'),
+)
+
+
+@pytest.mark.parametrize('original_filename, thumbnail_path', testdata)
+def test_create_thumbnail(original_filename, thumbnail_path, tmp_path):
     # Copy testing file to temp directory
     test_orig = Path(__file__).parent / 'annie-spratt-NlcSjubZ9tM-unsplash.jpg'
     main_path = Path(shutil.copy(str(test_orig), str(tmp_path)))
